ffb9dd5e0a33d2f36156a953a7353b757a444314
Skyler Dowdy <skylerwebdev@gmail.com>
2020-01-13 12:41:14 -0500

    Added Private Route

--- reactJS/skylerwebdev-reactjs-tutorial/src/configs/AppRouter.jsx
+++ reactJS/skylerwebdev-reactjs-tutorial/src/configs/AppRouter.jsx
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
+import PrivateRoute from './PrivateRoute'
 const AppRouter = () => {
     return (
         <div>
             <Switch>
             <Route exact path='/'><Home/></Route>
             <Route path='/about'><About/></Route>
-            <Route path='/contact'><Contact/></Route>
+            <PrivateRoute path='/contact' component={Contact}></PrivateRoute>
             </Switch>
         </div>
     )
